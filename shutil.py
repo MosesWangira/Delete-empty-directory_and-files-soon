7342e154e8959b9912a703d389dc635ca9544868
Moses Ouma Wangira <40572173+MosesWangira@users.noreply.github.com>
2019-12-13 17:14:25 +0300

    updated easy deletion
    provide local disk volume letter and delete all empty directories

--- shutil.py
+++ shutil.py
@@ -4,9 +4,33 @@ from os import path
 
 #show list of all directories in labelled volume
 print("************************************************************************")
-directories = os.listdir(r"I:")
+volume_name = input("Enter Local disk letter to check : ")
+directories = os.listdir(r"{0}:".format(volume_name))
 print("\n".join(directories))
 print("************************************************************************")
+
+
+#listing files using for loop
+print("*****************************************")
+path_dir = os.listdir(r"{}:".format(volume_name))
+for files in path_dir:
+    count = 0
+    #checking if directory is empty
+    try:
+       if not os.listdir(r"{0}:\{1}".format(volume_name,files)):
+           #print("directory empty")
+           #print("directory is emty !!Deleting!! ")
+           #print("*************************")
+           shutil.rmtree(r"{0}:\{1}".format(volume_name,files))
+    except NotADirectoryError:
+        #print("not a directory")
+        continue
+    except PermissionError:
+        continue
+    except FileNotFoundError:
+        continue
+print("Empty folders deleted or no empty folders exist")
+print("*****************************************")
 
 
 #deletes empty directories function
@@ -36,7 +60,7 @@ def deleteEmptyDirectories():
 
 
 #calling delete empty directory function
-deleteEmptyDirectories()
+#deleteEmptyDirectories()
 
 
 
